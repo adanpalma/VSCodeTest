9b1be2e6467d02bc69a1ce6b8487d7c00e7ad8cc
adan.palma <adanpalma@hotmail.com>
2021-01-16 19:47:35 -0500

    remove some unused code

--- primerospasosvscode.py
+++ primerospasosvscode.py
@@ -1,10 +1,4 @@
-import math
-import os
-import sys
-from os import rename
-
 import requests
-
 
 respuesta = requests.get("https://www.lahipotecaria.com/")
 
@@ -12,10 +6,3 @@ if respuesta.status_code == 200:
     print("La pagina esta arriba " + str(respuesta.status_code))
 else:
     print("la pagina esta caida " + str(respuesta.status_code))
-
-
-hola
-
-
-def hola():
-    print("hola" * 20)
