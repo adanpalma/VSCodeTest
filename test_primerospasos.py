83e2820b0aaee25184a82321f46c498f4253418c
adan.palma <adanpalma@hotmail.com>
2021-01-18 22:19:45 -0500

    Agregue sub a calc.py y test test_sub a TestCalc

--- test_primerospasos.py
+++ test_primerospasos.py
@@ -5,3 +5,6 @@ import calc
 class TestCalc(unittest.TestCase):
     def test_add(self):
         self.assertEqual(calc.add(5, 5), 10)
+
+    def test_sub(self):
+        self.assertEqual(calc.sub(5, 4), 1)
