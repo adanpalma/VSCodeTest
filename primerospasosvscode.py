ba39571b627b01ab3f92f4bdc3caf9d9cbea79bf
adan.palma <adanpalma@hotmail.com>
2021-01-23 18:28:38 -0500

    prueba de commit

--- primerospasosvscode.py
+++ primerospasosvscode.py
@@ -1,8 +1,7 @@
 import requests
 
 respuesta = requests.get("https://www.lahipotecaria.com/")
-
 if respuesta.status_code == 200:
     print("La pagina esta arriba " + str(respuesta.status_code))
 else:
-    print("la pagina esta caida " + str(respuesta.status_code))
+    pass
