e09d8830e3ec27e5a1e20d273a87d2e23cda9600
adan.palma <adanpalma@hotmail.com>
2021-01-24 22:04:41 -0500

    nuevos test a a calc

--- test_primerospasos.py
+++ test_primerospasos.py
@@ -5,6 +5,8 @@ import calc
 class TestCalc(unittest.TestCase):
     def test_add(self):
         self.assertEqual(calc.add(5, 5), 10)
+        self.assertEqual(calc.add(-5, 4), -1)
 
     def test_sub(self):
         self.assertEqual(calc.sub(5, 4), 1)
+        self.assertEqual(calc.sub(1, -1), 2)
